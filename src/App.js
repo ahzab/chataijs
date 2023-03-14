807d6f2062e883dee529949dd908d05139dacf57
Charles Holtzkampf <charles.holtzkampf@gmail.com>
2023-03-14 13:16:12 +0000

    websockets update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,182 @@
 import logo from './logo.svg';
-import './App.css';
+import './styles/globals.css';
+import { useState, useRef, useEffect } from 'react'
+import ReactMarkdown from 'react-markdown'
+import CircularProgress from '@mui/material/CircularProgress';
+import styles from './styles/Home.module.css'
+import user from './assets/usericon.png'
+import parrot from './assets/parroticon.png'
 
-function App() {
+const App = () => {
+  const [userInput, setUserInput] = useState("");
+  const [history, setHistory] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [streamText, setStreamText] = useState('');
+  const [messages, setMessages] = useState([
+    {
+      "message": "Hi there! How can I help?",
+      "type": "apiMessage"
+    }
+  ]);
+  const messageListRef = useRef(null);
+  const textAreaRef = useRef(null);
+
+  // Auto scroll chat to bottom
+  useEffect(() => {
+    const messageList = messageListRef.current;
+    messageList.scrollTop = messageList.scrollHeight;
+  }, [messages]);
+
+  // Focus on text field on load
+  useEffect(() => {
+    textAreaRef.current.focus();
+  }, []);
+
+  // Handle errors
+  const handleError = () => {
+    setMessages((prevMessages) => [...prevMessages, { "message": "Oops! There seems to be an error. Please try again.", "type": "apiMessage" }]);
+    setLoading(false);
+    setUserInput("");
+  }
+
+   // Handle form submission
+   const handleSubmit = async(e) => {
+    e.preventDefault();
+
+    if (userInput.trim() === "") {
+      return;
+    }
+
+    setLoading(true);
+    setMessages((prevMessages) => [...prevMessages, { "message": userInput, "type": "userMessage" }]);
+
+    // Send user question and history to API
+    const socket = new WebSocket('ws://localhost:9000/chat');
+
+    socket.addEventListener('open', (event) => {
+      console.log('WebSocket connected');
+      
+      const message = JSON.stringify({ question: userInput, history: history });
+      socket.send(message);
+    });
+    
+    socket.addEventListener('message', (event) => {
+      console.log(`Received message: ${event.data}`);
+      // Update the state with the new streaming text
+      setMessages(prevMessages => prevMessages + event.data.message);
+      
+      // Handle the response from the server here
+    });
+    
+    socket.addEventListener('error', (event) => {
+      console.error('WebSocket error', event);
+      
+      // Handle the error here
+    });
+    
+    socket.addEventListener('close', (event) => {
+      console.log('WebSocket disconnected', event);
+      
+      // Handle the close event here
+    });
+
+    // Reset user input
+    setUserInput("");
+    const data = await socket.json();
+
+    if (data.result.error === "Unauthorized") {
+      handleError();
+      return;
+    }
+
+    setMessages((prevMessages) => [...prevMessages, { "message": data.result.success, "type": "apiMessage" }]);
+    setLoading(false);
+    
+  };
+  // Prevent blank submissions and allow for multiline input
+  const handleEnter = (e) => {
+    if (e.key === "Enter" && userInput) {
+      if(!e.shiftKey && userInput) {
+        handleSubmit(e);
+      }
+    } else if (e.key === "Enter") {
+      e.preventDefault();
+    }
+  };
+
+  // Keep history in sync with messages
+  useEffect(() => {
+    if (messages.length >= 3) {
+      setHistory([[messages[messages.length - 2].message, messages[messages.length - 1].message]]);
+    }
+    }, [messages])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={styles.topnav}>
+        <div className = {styles.navlogo}>
+            <a href="/">LangChain</a>
+          </div>
+        <div className = {styles.navlinks}>
+          <a href="https://langchain.readthedocs.io/en/latest/" target="_blank">Docs</a>
+            <a href="https://github.com/zahidkhawaja/langchain-chat-nextjs" target="_blank">GitHub</a>
+        </div>
+      </div>
+   <main className={styles.main}>
+      <div className = {styles.cloud}>
+        <div ref={messageListRef} className = {styles.messagelist}>
+        {messages.map((message, index) => {
+          return (
+            // The latest message sent by the user will be animated while waiting for a response
+              <div key = {index} className = {message.type === "userMessage" && loading && index === messages.length - 1  ? styles.usermessagewaiting : message.type === "apiMessage" ? styles.apimessage : styles.usermessage}>
+                {/* Display the correct icon depending on the message type */}
+                {message.type === "apiMessage" ? <img src = {parrot} alt = "AI" width = "30" height = "30" className = {styles.boticon} priority = {true} /> : <img src = {user} alt = "Me" width = "30" height = "30" className = {styles.usericon} priority = {true} />}
+              <div className = {styles.markdownanswer}>
+                {/* Messages are being rendered in Markdown format */}
+                <ReactMarkdown linkTarget = {"_blank"}>{message.message}</ReactMarkdown>
+                </div>
+              </div>
+          )
+        })}
+        </div>
+            </div>
+           <div className={styles.center}>
+            
+            <div className = {styles.cloudform}>
+           <form onSubmit = {handleSubmit}>
+          <textarea 
+          disabled = {loading}
+          onKeyDown={handleEnter}
+          ref = {textAreaRef}
+          autoFocus = {false}
+          rows = {1}
+          maxLength = {512}
+          type="text" 
+          id="userInput" 
+          name="userInput" 
+          placeholder = {loading? "Waiting for response..." : "Type your question..."}  
+          value = {userInput} 
+          onChange = {e => setUserInput(e.target.value)} 
+          className = {styles.textarea}
+          />
+            <button 
+            type = "submit" 
+            disabled = {loading}
+            className = {styles.generatebutton}
+            >
+            {loading ? <div className = {styles.loadingwheel}><CircularProgress color="inherit" size = {20}/> </div> : 
+            // Send icon SVG in input field
+            <svg viewBox='0 0 20 20' className={styles.svgicon} xmlns='http://www.w3.org/2000/svg'>
+            <path d='M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z'></path>
+          </svg>}
+            </button>
+            </form>
+            </div>
+            <div className = {styles.footer}>
+            <p>Powered by <a href = "https://github.com/hwchase17/langchain" target="_blank">LangChain</a>. Built by <a href="https://www.sentnl.io" target="_blank">Sentnl</a>.</p>
+            </div>
+        </div>
+      </main>
     </div>
   );
 }
